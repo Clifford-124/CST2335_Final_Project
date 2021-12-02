cf5865dd38f7e1d300ec0e50b26196d010f7df9d
Clifford-124 <75549387+Clifford-124@users.noreply.github.com>
2021-12-02 14:25:24 -0500

    Saved Data and added javadocs

--- app/src/main/java/com/project/cst2335/Activities/CarModelDetailActivity.java
+++ app/src/main/java/com/project/cst2335/Activities/CarModelDetailActivity.java
@@ -1,21 +1,50 @@
 package com.project.cst2335.Activities;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.project.cst2335.Fragments.CarModelDetailFragment;
+import com.project.cst2335.Fragments.PhotoListFragment;
+import com.project.cst2335.Models.CarModel;
 import com.project.cst2335.R;
 import com.project.cst2335.Utils.Constants;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * CarModelDetail activity responsible to display
+ * selected model estimates
+ *
+ * @author Cliff
+ * @version 1.0
+ */
 public class CarModelDetailActivity extends AppCompatActivity {
 
+    //Buttons
     Button saveButton,deleteButton,closeButton;
 
     @Override
@@ -23,6 +52,7 @@ public class CarModelDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_model_detail);
 
+        //Setting Toolbar
         Toolbar myToolbar = findViewById(R.id.toolbar_detail);
 
         String activity = getResources().getString(R.string.carbon_dioxide_activity_main);
@@ -34,7 +64,7 @@ public class CarModelDetailActivity extends AppCompatActivity {
 
         setSupportActionBar(myToolbar);
 
-        //estimateDetails = (TextView) findViewById(R.id.esti);
+        //Fetch Data
         String model_id = getIntent().getStringExtra(Constants.ARG_MODEL_ID);
         String model_name = getIntent().getStringExtra(Constants.ARG_MODEL_NAME);
         String display = getIntent().getStringExtra(Constants.ARG_DISPLAY);
@@ -42,44 +72,14 @@ public class CarModelDetailActivity extends AppCompatActivity {
         String distance_unit = getIntent().getStringExtra(Constants.ARG_DISTANCE_UNIT);
         String vehicle_nake = getIntent().getStringExtra(Constants.ARG_VEHICLE_MAKE);
 
-        Toast.makeText(this,model_id , Toast.LENGTH_SHORT).show();
-
-        saveButton = findViewById(R.id.saveEstimate);
-        closeButton = findViewById(R.id.closeEstimate);
-        deleteButton = findViewById(R.id.deleteEstimate);
-
-        if (display.contentEquals("fromDB")) {
-            deleteButton.setVisibility(View.VISIBLE);
-            saveButton.setVisibility(View.GONE);
-        } else {
-            deleteButton.setVisibility(View.GONE);
-            saveButton.setVisibility(View.VISIBLE);
-        }
-
-        closeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //DB Operations
-            }
-        });
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //DB Operations
-            }
-        });
+        //Load Fragment and pass data
         CarModelDetailFragment detailFragment =
                 CarModelDetailFragment.newInstance(model_id,
                         model_name,
                         display,
                         distance,
                         distance_unit,vehicle_nake);
+
 
         FragmentManager fMgr = getSupportFragmentManager();
         FragmentTransaction tx = fMgr.beginTransaction();
